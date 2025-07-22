e3c3d65d1cd7bb102d27a8bc5cb4c17f1a660f4b
Md Rafsun <rsrafusn07@gmail.com>
2025-07-22 22:36:17 +0600

    Updated navbar layout and styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import './Navbar.css';
-import logo from '../../assets/logo.png';
+import { FaPhone, FaEnvelope, FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <img src={logo} alt="Logo" className="navbar-logo" />
+    <>
+      {/* Top Info Bar */}
+      <div className="topbar">
+        <div className="topbar-left">
+          <span><FaPhone /> +880 1234-567890</span>
+          <span><FaEnvelope /> info@sahajgonit.com</span>
+        </div>
+        <div className="topbar-right">
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaInstagram /></a>
+          <a href="#"><FaTwitter /></a>
+          <a href="#"><FaYoutube /></a>
+        </div>
       </div>
-      <ul className="navbar-links">
-        <li>হোম</li>
-        <li>লেকচার</li>
-        <li>বই এবং পিডিএফ</li>
-        <li>গ্রাফ ক্যালকুলেটর</li>
-      </ul>
-      <div className="navbar-auth">
-        <button className="login-btn">Login</button>
-        <button className="signup-btn">Sign Up</button>
-      </div>
-    </nav>
+
+      {/* Main Navbar */}
+      <nav className="navbar">
+        <ul className="navbar-links">
+          <li>হোম</li>
+          <li>লেকচার</li>
+          <li>বই এবং পিডিএফ</li>
+          <li>গ্রাফ ক্যালকুলেটর</li>
+        </ul>
+        <div className="navbar-auth">
+          <button className="login-btn">Login</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </nav>
+    </>
   );
 };
 
